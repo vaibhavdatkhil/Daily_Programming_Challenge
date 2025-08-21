481eaecf9e512da9263fde7a7218c4ef2faaa0f3
vaibhavdatkhil <vaibhavdatkhil@gmail.com>
2025-08-21 23:16:01 +0530

    Add files via upload

---
+++ dayseven.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int trap(vector<int>& height) {
+    int left = 0, right = height.size() - 1;
+    int leftMax = 0, rightMax = 0;
+    int water = 0;
+
+    while (left < right) {
+        if (height[left] < height[right]) {
+            if (height[left] >= leftMax) {
+                leftMax = height[left];
+            } else {
+                water += leftMax - height[left];
+            }
+            left++;
+        } else {
+            if (height[right] >= rightMax) {
+                rightMax = height[right];
+            } else {
+                water += rightMax - height[right];
+            }
+            right--;
+        }
+    }
+    return water;
+}
+
+int main() {
+    vector<int> height = {0,1,0,2,1,0,1,3,2,1,2,1};
+    cout << "Trapped Water: " << trap(height) << endl;
+    return 0;
+}
