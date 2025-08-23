7bf9a069d6ab794c6f2aded9c20b3a7866c87fee
vaibhavdatkhil <vaibhavdatkhil@gmail.com>
2025-08-23 21:19:26 +0530

    Add files via upload

---
+++ daynine.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+string longestCommonPrefix(vector<string>& strs) {
+    if (strs.empty()) return "";
+
+    string prefix = strs[0];  // take the first string as initial prefix
+    for (int i = 1; i < strs.size(); i++) {
+        // shrink prefix until it matches strs[i]
+        while (strs[i].find(prefix) != 0) {
+            prefix = prefix.substr(0, prefix.size() - 1);
+            if (prefix.empty()) return "";
+        }
+    }
+    return prefix;
+}
+
+int main() {
+    vector<string> strs = {"flower", "flow", "flight"};
+    cout << "Longest Common Prefix: " << longestCommonPrefix(strs) << endl;
+    return 0;
+}
